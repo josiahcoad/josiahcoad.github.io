2e1757af1c6905b2d043ef4ea0c0dabcaead17c2
Josiah Coad <josiah@coad.net>
2017-04-01 14:21:15 -0500

    index

--- js/main.js
+++ js/main.js
@@ -80,9 +80,27 @@ $(function() {
 });
 
 
+/* ========================================================================= */
+/*  Text Rotation
+/* ========================================================================= */
+
+var terms = $("#helloworlds li");
+
+function rotateTerm() {
+
+ var ct = $("#rotate").data("term") || 0;
+
+ console.log(terms.eq([ct]).text());
+ 
+  $("#rotate").data("term", 
+    ct == terms.length -1 ? 0 : ct + 1).text(terms.eq([ct]).text())
+  .fadeIn().delay(3000).fadeOut(300, rotateTerm);
+
+}
 
 $(document).ready(function(){
 
+    $(rotateTerm);
 	/* ========================================================================= */
 	/*	Menu item highlighting
 	/* ========================================================================= */
