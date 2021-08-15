74211bd4e9d3c4eef14baa0bb24bef9753c11f1c
Josiah Coad <josiah@coad.net>
2021-08-15 01:00:38 -0500

    added share, dropdown and love incon to list card

--- project_travel_advisor/src/components/PlaceDetails/PlaceDetails.js
+++ project_travel_advisor/src/components/PlaceDetails/PlaceDetails.js
@@ -3,6 +3,13 @@ import { Box, Typography, Button, Card, CardMedia, CardContent, CardActions, Chi
 import LocationOnIcon from '@material-ui/icons/LocationOn';
 import PhoneIcon from '@material-ui/icons/Phone';
 import Rating from '@material-ui/lab/Rating';
+import Collapse from '@material-ui/core/Collapse';
+// eslint-disable-next-line
+import clsx from 'clsx';
+import IconButton from '@material-ui/core/IconButton';
+import FavoriteIcon from '@material-ui/icons/Favorite';
+import ShareIcon from '@material-ui/icons/Share';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 
 import useStyles from './styles.js';
 
@@ -21,6 +28,11 @@ const mkDirectionsUrl = (address, coords) => `https://www.google.com/maps/dir/${
 const PlaceDetails = ({ place, selected, refProp, coords }) => {
   if (selected) refProp?.current?.scrollIntoView({ behavior: 'smooth', block: 'start' });
   const classes = useStyles();
+  const [expanded, setExpanded] = React.useState(false);
+
+  const handleExpandClick = () => {
+    setExpanded(!expanded);
+  };
 
   return (
     <Card elevation={6}>
@@ -59,6 +71,51 @@ const PlaceDetails = ({ place, selected, refProp, coords }) => {
           </Box>
         ))}
         {place.deals.length > 5 && <Typography variant="subtitle2" color="textSecondary">+ {place.deals.length - 5} more!</Typography>}
+        <CardActions disableSpacing>
+          <IconButton aria-label="add to favorites">
+            <FavoriteIcon />
+          </IconButton>
+          <IconButton aria-label="share">
+            <ShareIcon />
+          </IconButton>
+          <IconButton
+            className={clsx(classes.expand, {
+              [classes.expandOpen]: expanded,
+            })}
+            onClick={handleExpandClick}
+            aria-expanded={expanded}
+            aria-label="show more"
+          >
+            <ExpandMoreIcon />
+          </IconButton>
+        </CardActions>
+        <Collapse in={expanded} timeout="auto" unmountOnExit>
+          <CardContent>
+            <Typography paragraph>Method:</Typography>
+            <Typography paragraph>
+              Heat 1/2 cup of the broth in a pot until simmering, add saffron and set aside for 10
+              minutes.
+            </Typography>
+            <Typography paragraph>
+              Heat oil in a (14- to 16-inch) paella pan or a large, deep skillet over medium-high
+              heat. Add chicken, shrimp and chorizo, and cook, stirring occasionally until lightly
+              browned, 6 to 8 minutes. Transfer shrimp to a large plate and set aside, leaving chicken
+              and chorizo in the pan. Add pimentón, bay leaves, garlic, tomatoes, onion, salt and
+              pepper, and cook, stirring often until thickened and fragrant, about 10 minutes. Add
+              saffron broth and remaining 4 1/2 cups chicken broth; bring to a boil.
+            </Typography>
+            <Typography paragraph>
+              Add rice and stir very gently to distribute. Top with artichokes and peppers, and cook
+              without stirring, until most of the liquid is absorbed, 15 to 18 minutes. Reduce heat to
+              medium-low, add reserved shrimp and mussels, tucking them down into the rice, and cook
+              again without stirring, until mussels have opened and rice is just tender, 5 to 7
+              minutes more. (Discard any mussels that don’t open.)
+            </Typography>
+            <Typography>
+              Set aside off of the heat to let rest for 10 minutes, and then serve.
+            </Typography>
+          </CardContent>
+        </Collapse>
         {place.gmap.types.map((type) => (
           <Chip key={type} size="small" label={type} className={classes.chip} />
         ))}
--- project_travel_advisor/src/components/PlaceDetails/styles.js
+++ project_travel_advisor/src/components/PlaceDetails/styles.js
@@ -1,6 +1,6 @@
 import { makeStyles } from '@material-ui/core/styles';
 
-export default makeStyles(() => ({
+export default makeStyles((theme) => ({
   chip: {
     margin: '5px 5px 5px 0',
   },
@@ -9,5 +9,15 @@ export default makeStyles(() => ({
   },
   spacing: {
     display: 'flex', alignItems: 'center', justifyContent: 'space-between',
+  },
+  expand: {
+    transform: 'rotate(0deg)',
+    marginLeft: 'auto',
+    transition: theme.transitions.create('transform', {
+      duration: theme.transitions.duration.shortest,
+    }),
+  },
+  expandOpen: {
+    transform: 'rotate(180deg)',
   },
 }));
