68164b445d3d585f31f1001d28ad5e94d658588f
Josiah Coad <josiah@coad.net>
2021-08-15 00:07:57 -0500

    removing the auto-reload on bounds/type changing

--- project_travel_advisor/src/App.js
+++ project_travel_advisor/src/App.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { CssBaseline, Grid } from '@material-ui/core';
 import { MuiThemeProvider, createTheme } from '@material-ui/core/styles';
 
-import { getPlacesData, getWeatherData } from './api/travelAdvisorAPI';
+import { getPlacesData } from './api/travelAdvisorAPI';
 import Header from './components/Header/Header';
 import List from './components/List/List';
 import Map from './components/Map/Map';
@@ -27,9 +27,9 @@ const App = () => {
   const [rating, setRating] = useState('');
 
   const [coords, setCoords] = useState({});
+  // eslint-disable-next-line
   const [bounds, setBounds] = useState(null);
 
-  const [weatherData, setWeatherData] = useState([]);
   const [filteredPlaces, setFilteredPlaces] = useState([]);
   const [places, setPlaces] = useState([]);
 
@@ -50,21 +50,16 @@ const App = () => {
   }, [rating]);
 
   useEffect(() => {
-    if (bounds) {
-      setIsLoading(true);
+    setIsLoading(true);
 
-      getWeatherData(coords.lat, coords.lng)
-        .then((data) => setWeatherData(data));
-
-      getPlacesData(type, bounds.sw, bounds.ne)
-        .then((data) => {
-          setPlaces(data.filter((place) => place.name && place.gmap.user_ratings_total > 0));
-          setFilteredPlaces([]);
-          setRating('');
-          setIsLoading(false);
-        });
-    }
-  }, [bounds, type]);
+    getPlacesData()
+      .then((data) => {
+        setPlaces(data.filter((place) => place.name && place.gmap.user_ratings_total > 0));
+        setFilteredPlaces([]);
+        setRating('');
+        setIsLoading(false);
+      });
+  }, []);
 
   const onLoad = (autoC) => setAutocomplete(autoC);
 
@@ -100,7 +95,6 @@ const App = () => {
               setCoords={setCoords}
               coords={coords}
               places={filteredPlaces.length ? filteredPlaces : places}
-              weatherData={weatherData}
             />
           </Grid>
         </Grid>
