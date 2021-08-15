18b6a54a711406ab4500bb2ee5349f91d86e1c00
Josiah Coad <josiah@coad.net>
2021-08-15 00:08:16 -0500

    removing weatherdata api call

--- project_travel_advisor/src/api/travelAdvisorAPI.js
+++ project_travel_advisor/src/api/travelAdvisorAPI.js
@@ -5,11 +5,10 @@ export const getPlacesData = async () => {
   try {
     const { data } = await axios.get('https://gbjt9fglc8.execute-api.us-west-2.amazonaws.com/api/todos');
     const placeData = data.map((place) => place.data);
+    console.log(placeData.length);
     return placeData;
   } catch (error) {
     console.log(error);
     return [];
   }
 };
-
-export const getWeatherData = async () => ({});
